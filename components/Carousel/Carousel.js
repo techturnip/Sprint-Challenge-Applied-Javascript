5418bcab162b69af0f78db3391cf2e5e679b5aca
Tyler Turnipseed <tyler@techturnip.us>
2019-05-11 14:13:02 -0400

    Uncommented Carousel code in HTML, added animate.css to <head> element, created Carousel class in Carousel.js, used this.currentIndex to use for indexing the this.imgs nodeList for display of each img, used event listeners on each button to increment and decrement the currentIndex used animate.css to add animation to the carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,43 @@
 class Carousel {
+    constructor(carousel) {
+        this.carousel = carousel;
+        this.leftBtn = this.carousel.querySelector('.left-button');
+        this.rightBtn = this.carousel.querySelector('.right-button');
+        this.imgs = this.carousel.querySelectorAll('img');
+        this.currentIndex = 0;
+        this.imgs[this.currentIndex].style.display = 'block';
+        this.imgs.forEach(img => img.classList.add('animated', 'fadeIn'));
+        this.rightBtn.addEventListener('click', () => this.cycleNextCarousel());
+        this.leftBtn.addEventListener('click', () => this.cyclePrevCarousel());
+    }
 
+    cycleNextCarousel() {
+        this.imgs.forEach(img => {
+            img.style.display = 'none'
+        });
+        if (this.currentIndex < this.imgs.length - 1) {
+            this.currentIndex++
+        } else {
+            this.currentIndex = 0;
+        }
+        this.imgs[this.currentIndex].style.display = 'block';
+
+      
+    }
+
+    cyclePrevCarousel() {
+        this.imgs.forEach(img => img.style.display = 'none');
+
+        if (this.currentIndex > 0) {
+            this.currentIndex--
+        } else {
+            this.currentIndex = this.imgs.length - 1;
+        }
+        this.imgs[this.currentIndex].style.display = 'block';
+    }
 }
 
-let carousel = document.querySelector();
+let carousel = document.querySelectorAll('.carousel').forEach(slider => new Carousel(slider));
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -2,11 +2,9 @@ class TabLink {
   constructor(tabElement){
     // assign this.tabElement to the tabElement DOM reference
     this.tabElement = tabElement;
-    console.log('this.tabElement:', this.tabElement);
 
     // Get the `data-tab` value from this.tabElement and store it here
     this.tabData = this.tabElement.dataset.tab;
-    console.log('tabData:', this.tabData) ;
     
     // We need to find out if a user clicked 'all' cards or a specific category.  Follow the instructions below to accomplish this task:    
     
@@ -21,7 +19,6 @@ class TabLink {
 
      // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class. 
     this.cards = Array.from(this.cards).map(card => new TabCard(card));
-    console.log('this.cards', this.cards)
 
     // Add a click event that invokes this.selectTab
     this.tabElement.addEventListener('click', () => this.selectTab());
